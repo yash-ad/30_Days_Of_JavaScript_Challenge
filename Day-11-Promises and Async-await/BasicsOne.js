c34e33e04be1afecb6ae77e7bd34afc4a87da3d9
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-08-08 13:46:39 +0530

    Interview prep and practice on code and questions

--- Day-11-Promises and Async-await/BasicsOne.js
+++ Day-11-Promises and Async-await/BasicsOne.js
@@ -25,6 +25,61 @@
 
 
 
+//.then() and .catch():-
+
+
+//1].What is .then()?
+//Well '.then()' method is used to handle the successful resolution of a promise and '.then()' is directly connected the resolve of a promise.
+
+
+//2].What is .catch()?
+//Well '.catch()' method is used to handle the rejection of a promise and '.catch()'  method is directly connected to the reject of a promise.
+
+
+//Syntax below:
+let myPromise = new Promise((resolve,reject) => {
+    
+    setTimeout(() => {
+        console.log("Hello I'm ans asynchronous code");
+        resolve("Data from the promise");
+        
+    },2000) //Set a delay for 2 seconds
+})
+.then((data) => {
+    console.log(data);
+    return "Processed data" //Return the data to the next data handler.
+    
+})
+.then((result) => {
+   console.log(result);
+    
+})
+.catch((error) => {
+    console.log(error.message);
+    
+})
+
+//What is the return value of '.then()' and '.catch()'?
+//Both '.then()' and '.catch()' return a promise .
+//this allows to chain promises together to handle resolution or rejection of one promise and then handle the successful or rejection of another promise.
+
+//What is callBack hell?
+//callBack hell is a situation where the lot of nested callbacks,which can make to code more difficult to read and maintain.
+
+//How can you use '.then()' and '.catch()' to avoid callback hell?
+ //Certainly using '.then()' and '.catch()' to chain promises together.which can help to avoid callback hell.
+//'.then()' is used for handling the successful resolution of one promise and then use another '.then()' handle the successful resolution of next promise.
+
+
+//Best practices for using '.then()' and '.catch()'?
+ //Always handle the rejection of a promise even if don't expect to be rejected.
+//Use '.then()' to handle chain promises together to avoid callback hells.
+ 
+
+
+//Why we do we use '.then()' and '.catch()' in promises:-
+//Using '.then()' and '.catch()' into the promises for handling successful resolution or a rejection of an asynchronous operation.
+//And it is a structure way of handling the asynchronous code.
 
 
 
