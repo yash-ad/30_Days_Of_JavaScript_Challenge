cecd232af706d1f709149b1dce3a1ec4f5a36c37
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-08-26 14:08:27 +0530

    Files and folders are arranged accordingly

--- Day-13-Modules/ProblemOne/BasicsOne.js
+++ Day-13-Modules/ProblemOne/BasicsOne.js
@@ -1,7 +1,3 @@
-///What is a module in javascript?
-//In javascript module is a self-contained piece of code that can be imported and reused in other files.
-//It helps to organize the code into smaller manageable units and making code easier to maintain and understand.
-
 
 
 
--- Day-13-Modules/ProblemOne/mathOperations.mjs
+++ Day-13-Modules/ProblemOne/mathOperations.mjs
@@ -1,3 +1,11 @@
+///What is a module in javascript?
+//In javascript module is a self-contained piece of code that can be imported and reused in other files.
+//It helps to organize the code into smaller manageable units and making code easier to maintain and understand.
+
+
+
+
+
 // 1. Exporting and Importing a Simple Function
 // Problem:
 
